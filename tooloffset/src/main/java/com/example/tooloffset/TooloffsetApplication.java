de4e2f179bf446ebb2a4a12c01e62b01a52de23b
MikSob4523 <mikolaisobolewski@gmail.com>
2023-06-04 03:37:14 +0200

    utworzono projekt spring z niezbędnymi dependencies

---
+++ tooloffset/src/main/java/com/example/tooloffset/TooloffsetApplication.java
@@ -0,0 +1,13 @@
+package com.example.tooloffset;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TooloffsetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TooloffsetApplication.class, args);
+	}
+
+}
---
+++ tooloffset/src/test/java/com/example/tooloffset/TooloffsetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tooloffset;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TooloffsetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
